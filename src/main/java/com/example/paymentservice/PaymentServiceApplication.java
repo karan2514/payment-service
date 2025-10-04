40d2dbd26b5f80d899c7afbd98dce4ab264168dd
karan2514 <karan2514singh@gmail.com>
2025-10-04 11:38:35 +0530

    updated war changes

--- src/main/java/com/example/paymentservice/PaymentServiceApplication.java
+++ src/main/java/com/example/paymentservice/PaymentServiceApplication.java
@@ -2,10 +2,17 @@ package com.example.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PaymentServiceApplication {
+public class PaymentServiceApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(PaymentServiceApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PaymentServiceApplication.class);
     }
 }
